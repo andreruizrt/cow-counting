71f651ddb52ac07ac451cf2067371755fff9f8bb
Andre Almeida <andrelucasruizalmeida@gmail.com>
2023-06-18 20:25:46 -0300

    new file handler functions

--- filehandler.py
+++ filehandler.py
@@ -1,6 +1,13 @@
 import os
 
-DIRETORIO_DEFAULT = "resource/Dataset/"
+import cv2
+import json
+
+
+
+RESOURCE_DEFAULT = "resource/"
+DIRETORIO_DEFAULT_DATASET = "Dataset/"
+DIRETORIO_DEFAULT_PREDICTIONS = "Predictions/"
 
 
 def getAllImagesName(diretorio):
@@ -21,3 +28,39 @@ def encontrarNomeImagem(pathToImage):
         return palavra
     else:
         return parts
+
+
+def anotarQuantidadeEncontrados(pathToImage, quantidade):
+    image = cv2.imread(pathToImage)
+
+    text = 'Bovinos: ' + str(quantidade)
+
+    position = (10, 120)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 5
+    font_color = (255, 255, 0)
+    line_type = 2
+
+    cv2.putText(image, text, position, font, font_scale, font_color, line_type)
+
+    cv2.imwrite(pathToImage, image)
+
+
+def redimensionarImagem(img):
+    scale_percent = 20
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    print('redimensionarImagem >> Dimensão original : ', img.shape)
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    print('redimensionarImagem >> Resized Dimensions : ', resized.shape)
+
+    return resized
+
+
+def salvarAquivoJson(caminhoArquivo, conteudo):
+    with open(caminhoArquivo, "w") as arquivo:
+        json.dump(conteudo, arquivo)
+
+
